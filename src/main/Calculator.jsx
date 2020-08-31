682274395f6e43550263dbc0561e48508a6fe159
Vinícius Amorim da Silva <amorimm.vinicius@gmail.com>
2020-08-31 15:17:57 -0300

    Implementando a Lógica 01

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,8 +4,17 @@ import './Calculator.css'
 import Button from '../components/Button';
 import Display from '../components/Display';
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
 
 export default class Calculator extends Component {
+
+    state = { ...initialState };
 
     constructor(props) {
 
@@ -17,7 +26,8 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        console.log('limpar');
+        this.setState({ ...initialState });
+        //console.log('limpar');
     }
 
     setOperation(operation) {
@@ -25,13 +35,31 @@ export default class Calculator extends Component {
     }
 
     addDigit(n) {
-        console.log(n);
+        if (n === '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue;
+        const displayValue = currentValue + n;
+        this.setState({ displayValue, clearDisplay: false });
+        //console.log(n);
+
+        if (n !== '.') {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [ ...this.state.values ]
+            values[i] = newValue;
+            this.setState({ values });
+            console.log(values);
+        }
     }
 
     render() {
         return (
             <div className="calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.setOperation} operation />
                 <Button label="7" click={this.addDigit} />
@@ -47,7 +75,7 @@ export default class Calculator extends Component {
                 <Button label="3" click={this.addDigit}/>
                 <Button label="+" click={this.setOperation} operation/>
                 <Button label="0" click={this.addDigit} double/>
-                <Button label="." click={this.setOperation} />
+                <Button label="." click={this.addDigit} />
                 <Button label="=" click={this.setOperation} operation />
             </div>
         )
